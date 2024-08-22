9938430295e6313b1dd20c84be7c86fd83d22089
Hashara13 <hnethmin13@gmail.com>
2024-08-23 00:00:50 +0530

    minor changes

--- src/test/java/com/rest/test/GetCallBDD.java
+++ src/test/java/com/rest/test/GetCallBDD.java
@@ -1,17 +1,20 @@
 package com.rest.test;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 public class GetCallBDD {
-@Test
-public void testCircuits() {
-	given().
-	when().
-	get("http://ergast.com/api/f1/2015/circuits.json").
-then().
-assertThat().
-body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
-	
-}
+    @Test
+    public void testCircuits() {
+        given().
+        when().
+        get("http://ergast.com/api/f1/2015/circuits.json").
+        then().
+        log().all().  
+        assertThat().
+        body("MRData.CircuitTable.Circuits.circuitId", notNullValue()). 
+        body("MRData.CircuitTable.Circuits.circuitId", hasSize(19)).and()
+        .header("current-length", equalTo("4551")); 
+    }
 }
