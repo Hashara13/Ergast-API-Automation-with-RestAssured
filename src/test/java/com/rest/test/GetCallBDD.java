d4bd98783ad46e505183054f5cc16edfc1c198ee
Hashara13 <hnethmin13@gmail.com>
2024-08-22 23:54:27 +0530

    circuit test case added

--- src/test/java/com/rest/test/GetCallBDD.java
+++ src/test/java/com/rest/test/GetCallBDD.java
@@ -1,5 +1,17 @@
 package com.rest.test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
 
 public class GetCallBDD {
-
+@Test
+public void testCircuits() {
+	given().
+	when().
+	get("http://ergast.com/api/f1/2015/circuits.json").
+then().
+assertThat().
+body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+	
+}
 }
